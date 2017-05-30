76b3470d33375b4670e99440791ad4a38018c7f5
yaeiji <s1240238@gmail.com>
2017-05-30 21:11:01 +0900

    first commit

---
+++ ClearDirector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class ClearDirector : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("GameScene");		
+		}
+	}
+}
